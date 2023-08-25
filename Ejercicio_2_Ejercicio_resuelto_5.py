5652785e2cec3c2d212427774d78dd84d86200f0
Alejandro Urrea Olarte <143052686+Alejo0407w@users.noreply.github.com>
2023-08-25 07:43:43 -0500

    Update Ejercicio_2_Ejercicio_resuelto_5.py

--- Ejercicio_2_Ejercicio_resuelto_5.py
+++ Ejercicio_2_Ejercicio_resuelto_5.py
@@ -1,8 +1,8 @@
 #Ejercicio 2: Ejercicio resuelto 5
-suma = 0    	#inicializa la variable suma en 0
-x = 20		#inicializa la variable x en 20
-suma = suma + x
-y = 40		#inicializa la variable y en 40
-x = x+y**2
-suma = suma + x/y
+suma = float(input("Añade el valor de 1 de la suma",))    	#Permite al usuario ingresar el valor 1 de la operacion
+x = float(input("Añade el valor 2 de la suma",))		#Permite al usuario ingresar el valor 2 de la operacion
+y = float(input("Añade eñ valor 3 de la operacion",))	#Permite al usuario ingresar el valor 3 de la operacion
+suma = suma + x     #Al valor que tiene la variable suma le añade el valor de x y actualiza la variable
+x = x+y**2          #Al valor de x le suma la variable y al cuadrado y actualiza el valor
+suma = suma + x/y   #Realiza la divison entre las variables x e y y les añade la variable suma
 print("el valor de la suma es", suma)
