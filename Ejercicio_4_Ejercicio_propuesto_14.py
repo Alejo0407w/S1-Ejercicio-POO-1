5601cc6a1776136c0f08c44bcd6347dfa8f432d0
Alejo0407w <143052686+Alejo0407w@users.noreply.github.com>
2023-08-24 22:26:35 -0500

    Update Ejercicio_4_Ejercicio_propuesto_14.py

--- Ejercicio_4_Ejercicio_propuesto_14.py
+++ Ejercicio_4_Ejercicio_propuesto_14.py
@@ -1,6 +1,6 @@
 #Ejercicio 4: Ejercicio propuesto 14
 
-num= float(input("ingrese un numero")) #permite al usuario ingresar el numero con el que desea trabajar
+num= float(input("ingrese un numero",)) #permite al usuario ingresar el numero con el que desea trabajar
 cuad = num**2 #eleva el numero ingresado al cuadrado
 cub= num**3	#eleva el numero ingresado al cubo
 
