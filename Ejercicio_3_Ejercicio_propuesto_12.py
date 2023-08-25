69be9569fa1aadf597d9d8791e44a3a0e444e152
Alejo0407w <143052686+Alejo0407w@users.noreply.github.com>
2023-08-24 22:25:47 -0500

    Update Ejercicio_3_Ejercicio_propuesto_12.py

--- Ejercicio_3_Ejercicio_propuesto_12.py
+++ Ejercicio_3_Ejercicio_propuesto_12.py
@@ -1,7 +1,7 @@
 #Ejercicio 3: Ejercicio propuesto 12
 horastrabajadas = float(input("ingrese el número de horas trabajadas ", )) #permite al usuario ingresar el número de horas
 salario = float(input("ingrese el valor de la hora ", )) #permite al usuario ingresar el valor de cada hora
-porcretenfuente = float(input(""))
+porcretenfuente = float(input("ingrese el valor de la retefuente en numeros decimales, separado por un punto",)) #permite al usuario ingresar el valor de la retefuente en decimal
 
 salariobruto = float(salario*horastrabajadas)
 retenfuente = float(salariobruto*porcretenfuente)
@@ -10,5 +10,4 @@ salarioneto = float(salariobruto-retenfuente)
 print("el valor del salario bruto es ", salariobruto)
 print("el valor de la retefuente es ", retenfuente)
 print("el valor del salario neto es ", salarioneto)
-
 
